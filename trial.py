b4fc9b363696bb8d17f72c721b8e58e3b0ca0d8c
Hamza Bin Mohammed Alasery <hamza.mohammed545@gmail.com>
2023-02-18 12:43:09 +0530

    Done

--- trial.py
+++ trial.py
@@ -2,9 +2,4 @@ print("Hello world")
 print("This is python")
 print("Line 3 by hamza")
 print("This is line three")
-<<<<<<< HEAD
-print("Line 4 on local")
-=======
 print("Line 4 on git")
->>>>>>> 145941d434a998608f99ee41730807ae5075dbb5
-
